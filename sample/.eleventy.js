2fd06fb21c41363144e11e709e578eee9cd2089d
Dafiul Haq <dafiulhaq97@gmail.com>
2020-08-16 18:42:32 +0700

    add passthroughcopy in sample

--- sample/.eleventy.js
+++ sample/.eleventy.js
@@ -2,8 +2,10 @@ const pluginTailwind = require("../index");
 
 module.exports = (eleventyConfig) => {
 
+    eleventyConfig.addPassthroughCopy({ "sample/assets/*.js": "." });
+
     eleventyConfig.addPlugin(pluginTailwind, {
-        src: "sample/main.css",
+        src: "sample/assets/style.css",
         configFile: "",
         autoprefixer: false,
         minify: false
---
+++ sample/assets/main.js
@@ -0,0 +1,3 @@
+setTimeout(() => {
+    document.getElementById("ex").innerHTML += "!!";
+}, 500);
