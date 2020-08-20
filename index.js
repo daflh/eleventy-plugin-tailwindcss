ac48b412fe41e326705168607e35ac81d101b527
Dafiul Haq <dafiulhaq97@gmail.com>
2020-08-20 16:51:38 +0700

    export configFunction directly

--- index.js
+++ index.js
@@ -2,21 +2,16 @@ const Eleventy = require("@11ty/eleventy");
 const shimmer = require("shimmer");
 const processor = require("./src/processor");
 
-module.exports = {
-    initArguments: {},
-    configFunction: (__, options = {}) => {
-        setImmediate(function () {
-
-            shimmer.wrap(Eleventy.prototype, "write", function (original) {
-                return function () {
-                    if (!this.isDryRun) {
-                        processor.call(this, options);
-                    }
-                    shimmer.unwrap(this, "write");
-                    return original.apply(this, arguments);
+module.exports = (__, options = {}) => {
+    setImmediate(function () {
+        shimmer.wrap(Eleventy.prototype, "write", function (original) {
+            return function () {
+                if (!this.isDryRun) {
+                    processor.call(this, options);
                 }
-            });
-
+                shimmer.unwrap(this, "write");
+                return original.apply(this, arguments);
+            }
         });
-    }
+    });
 }
