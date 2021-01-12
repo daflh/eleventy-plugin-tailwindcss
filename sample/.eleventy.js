27ecf688806ce86e2415398be9fb58066f9d166e
Dafiul Haq <dafiulhaq97@gmail.com>
2021-01-12 07:44:50 +0700

    eslint whitelist dot files

--- sample/.eleventy.js
+++ sample/.eleventy.js
@@ -1,17 +1,17 @@
-const pluginTailwind = require("../");
+const pluginTailwind = require('../');
 
 module.exports = (eleventyConfig) => {
-    eleventyConfig.addPassthroughCopy({ "sample/assets/*.js": "." });
-    
-    eleventyConfig.addPlugin(pluginTailwind, {
-        src: ["**/*"],
-        dest: ".",
-        configFile: "sample/tailwind.config.js",
-        watchEleventyWatchTargets: false,
-        keepFolderStructure: false,
-        autoprefixer: true,
-        autoprefixerOptions: {},
-        minify: true,
-        minifyOptions: {}
-    });
-}
+  eleventyConfig.addPassthroughCopy({ 'sample/assets/*.js': '.' });
+
+  eleventyConfig.addPlugin(pluginTailwind, {
+    src: ['**/*'],
+    dest: '.',
+    configFile: 'sample/tailwind.config.js',
+    watchEleventyWatchTargets: false,
+    keepFolderStructure: false,
+    autoprefixer: true,
+    autoprefixerOptions: {},
+    minify: true,
+    minifyOptions: {}
+  });
+};
